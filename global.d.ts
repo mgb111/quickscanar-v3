1fd9abe5f5ebeda77a451b731e373db6877dcb4c
mgb111 <manishbhanushali1101@gmail.com>
2025-07-28 19:15:37 +0530

    Update global.d.ts

--- global.d.ts
+++ global.d.ts
@@ -3,18 +3,18 @@ import React from "react";
 declare global {
   namespace JSX {
     interface IntrinsicElements {
-      "a-scene": React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement>;
-      "a-assets": React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement>;
-      "a-asset-item": React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement>;
-      "a-camera": React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement>;
-      "a-entity": React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement>;
-      "a-plane": React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement>;
-      "a-box": React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement>;
-      "a-sphere": React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement>;
-      "a-cylinder": React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement>;
-      "a-sky": React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement>;
-      "a-light": React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement>;
-      "a-animation": React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement>;
+      "a-scene": any;
+      "a-assets": any;
+      "a-asset-item": any;
+      "a-camera": any;
+      "a-entity": any;
+      "a-plane": any;
+      "a-box": any;
+      "a-sphere": any;
+      "a-cylinder": any;
+      "a-sky": any;
+      "a-light": any;
+      "a-animation": any;
       // Add more A-Frame elements as needed
     }
   }
